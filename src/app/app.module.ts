1d250d5547c04fd16d4aa81f1bb95999b6b232d4
GitMonies <jarod.smelosky@yahoo.com>
2023-10-26 20:25:17 -0500

    Added Date Selection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TasklistComponent } from './tasklist/tasklist.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     TasklistComponent,
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
