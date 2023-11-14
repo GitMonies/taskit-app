b05b4fdfa78bf51d594363426a4991c54f32384e
GitMonies <jarod.smelosky@yahoo.com>
2023-11-13 20:25:15 -0600

    Implemented CSS Styling into Modal Box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatNativeDateModule} from '@angular/material/core';
+import { TaskCreationModalComponent } from './task-creation-modal/task-creation-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     TasklistComponent,
+    TaskCreationModalComponent,
    ],
   imports: [
     BrowserModule,
---
+++ src/app/task-creation-modal/task-creation-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-creation-modal',
+  templateUrl: './task-creation-modal.component.html',
+  styleUrls: ['./task-creation-modal.component.css']
+})
+export class TaskCreationModalComponent {
+
+}
